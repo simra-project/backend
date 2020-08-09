e6a19d68599f038ca9059cc013e862e9a563bc3a
Daniel Habenicht <daniel-habenicht@outlook.de>
2020-08-09 00:02:51 +0000

    remove old dockerfiles

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-/usr/sbin/sshd
-
-printf "hash_prefix=${API_SECRET}\n" > simRa_security.config
-printf "critical=false\nnewestAppVersion=34\nurlToNewestAPK=https://example.org" > simRa_backend.config
-java -jar ./app.jar
